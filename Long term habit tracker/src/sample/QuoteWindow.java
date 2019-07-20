3c9fc9d05e5d30a7fdd2d0c07299a7a644637fd9
twistercool <40502071+twistercool@users.noreply.github.com>
2019-07-20 23:12:56 +0100

    Add files via upload

--- Long term habit tracker/src/sample/QuoteWindow.java
+++ Long term habit tracker/src/sample/QuoteWindow.java
@@ -16,18 +16,19 @@ public class QuoteWindow {
 
     private GridPane quotePane = new GridPane();
     private String quote;
-    private ArrayList<String> quoteList = new ArrayList();
+    private ArrayList<String> quoteList = new ArrayList<>();
 
     public QuoteWindow()
     {
 
         LoadQuotes();
-        quote = "\"" + quoteList.get(2) + "\"";  //sets the quote to be displayed and puts quotation marks around
+        quote = "\"" + quoteList.get(2) + "\"";
         Label quoteLabel = new Label(quote); //have to find another way to choose the quote, it's arbitrary here
 
 
         quotePane.add(quoteLabel, 1 , 1);
-        quotePane.setMinSize(50, 25);
+        quotePane.setMinSize(100, 75);
+
 
     }
 
@@ -39,13 +40,14 @@ public class QuoteWindow {
 
     public void LoadQuotes() {
         System.out.print("Begin loading quote dataset...");
-        int nbOfQuotes = 0;  //for counting the loaded quotes
+        int nbOfQuotes = 0;
         try{
             URL url = getClass().getResource("Quote_data.csv");  //finds the csv file
-            CSVReader reader = new CSVReader(new FileReader(new File(url.toURI()).getAbsolutePath()));  // initialises the reader to the file
-            String [] line;  //array of string of a single line in the csv file
-            reader.readNext();  //skip the first row (column headers)
-            while ((line = reader.readNext()) != null) {  //if the line is empty it stops reading
+            CSVReader reader = new CSVReader(new FileReader(new File(url.toURI()).getAbsolutePath())); // initialises the reader to the file
+            String [] line;
+            //skip the first row (column headers)
+            reader.readNext();
+            while ((line = reader.readNext()) != null) {
                 String currentQuote = line[0];
                 quoteList.add(currentQuote);
                 nbOfQuotes++;
